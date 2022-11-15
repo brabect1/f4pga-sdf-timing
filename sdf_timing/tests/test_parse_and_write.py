8863ee8f7003b5a61a7533de7662c0f0bc9f6f2e
Tomas Brabec <xtbr123@gmail.com>
2022-11-15 21:09:11 +0100

    More parse & write tests.

--- sdf_timing/tests/test_parse_and_write.py
+++ sdf_timing/tests/test_parse_and_write.py
@@ -28,11 +28,15 @@ from .test_syntax_elements import NullLogger, reconfigure, parse
 
 # Defines a data set to be tested. The structure is a list of records,
 # where each record is a 3-element list such that 1st element is a description,
-# 2nd element is the input data and 3rd element is the expected data.
+# 2nd element is the input (SDF) data and 3rd element is the expected data
+# (i.e. SDF after parsing and writing out).
 # Hence::
 #
 #     [ [<desc>, <intput>, <expected>], [<desc>, <intput>, <expected>], ...]
 #
+# Leading whitespace, trailing whitespace and empty lines are trimmed from
+# the expected data before comparison with actuals (to make comparison more
+# accurate).
 testdata = [
 ['minimum_sdf',
 '''(DELAYFILE (SDFVERSION "3.0"))''',
@@ -47,10 +51,116 @@ testdata = [
 (DELAYFILE
   // some comment
   (SDFVERSION "3.0")
-)''',
-'''\
+)''','''\
 (DELAYFILE
   (SDFVERSION "3.0")
+)'''
+],
+['full header',
+'''\
+(DELAYFILE
+(SDFVERSION "3.0")
+(DESIGN "BIGCHIP")
+(DATE "March 12, 1995 09:46")
+(VENDOR "Southwestern ASIC")
+(PROGRAM "Fast program")
+(VERSION "1.2a")
+(DIVIDER /)
+(VOLTAGE 5.5:5.0:4.5)
+(PROCESS "best:nom:worst")
+(TEMPERATURE -40:25:125)
+(TIMESCALE 100 ps)
+)''','''\
+(DELAYFILE
+(SDFVERSION "3.0")
+(DESIGN "BIGCHIP")
+(DATE "March 12, 1995 09:46")
+(VENDOR "Southwestern ASIC")
+(PROGRAM "Fast program")
+(VERSION "1.2a")
+(DIVIDER /)
+(VOLTAGE 5.5:5:4.5)
+(PROCESS "best:nom:worst")
+(TEMPERATURE -40:25:125)
+(TIMESCALE 100 ps)
+)'''
+],
+['conditional delays',
+'''\
+(DELAYFILE
+	(SDFVERSION "2.1")
+	(TIMESCALE 1 ps)
+
+	(CELL
+	(CELLTYPE "routing_bel")
+	(INSTANCE slicem/lut_c)
+    (DELAY
+	  (ABSOLUTE
+	    (COND B==1'b0&&C==1'b0&&D==1'b0 (IOPATH A Z  (61.500:100.000:150.900) (26.900:40.000:61.400)))
+	    (COND A==1'b0&&C==1'b0&&D==1'b0 (IOPATH B Z  (58.900:95.100:141.900) (24.500:36.000:55.800)))
+	    (COND A==1'b0&&B==1'b0&&D==1'b0 (IOPATH C Z  (59.900:95.600:141.100) (24.200:35.200:55.000)))
+	    (COND A==1'b0&&B==1'b0&&C==1'b0 (IOPATH D Z  (62.700:100.900:149.700) (26.100:38.300:59.900)))
+	  )
+    )
+	)
+)
+''','''\
+(DELAYFILE
+(SDFVERSION "2.1")
+(TIMESCALE 1 ps)
+(CELL
+  (CELLTYPE "routing_bel")
+  (INSTANCE slicem/lut_c)
+  (DELAY
+    (ABSOLUTE
+      (COND B == 1'b0 && C == 1'b0 && D == 1'b0 (IOPATH A Z (61.5:100:150.9)(26.9:40:61.4)))
+      (COND A == 1'b0 && C == 1'b0 && D == 1'b0 (IOPATH B Z (58.9:95.1:141.9)(24.5:36:55.8)))
+      (COND A == 1'b0 && B == 1'b0 && D == 1'b0 (IOPATH C Z (59.9:95.6:141.1)(24.2:35.2:55)))
+      (COND A == 1'b0 && B == 1'b0 && C == 1'b0 (IOPATH D Z (62.7:100.9:149.7)(26.1:38.3:59.9)))
+    )
+  )
+)
+)'''
+],
+['real tripple',
+'''\
+(DELAYFILE
+    (SDFVERSION "3.0")
+    (TIMESCALE 1ps)
+
+    (CELL
+        (CELLTYPE "MY_CELL")
+        (INSTANCE MY_INST)
+        (DELAY
+            (ABSOLUTE
+                (IOPATH A O (1:2:3)(4:5:6)(7:8:9))
+                (IOPATH B O (1:2:3)(4:5:6))
+                (IOPATH C O (1:2:3)())
+                (IOPATH D O (1:2:3))
+                (IOPATH E O (1) () (3))
+                (IOPATH F O () () ())
+            )
+        )
+    )
+)
+''','''\
+(DELAYFILE
+(SDFVERSION "3.0")
+(TIMESCALE 1 ps)
+(CELL
+  (CELLTYPE "MY_CELL")
+  (INSTANCE MY_INST)
+  (DELAY
+    (ABSOLUTE
+      (IOPATH A O (1:2:3)(4:5:6)(7:8:9))
+      (IOPATH B O (1:2:3)(4:5:6))
+      (IOPATH C O (1:2:3)())
+      (IOPATH D O (1:2:3))
+      (IOPATH E O (1)()(3))
+      (IOPATH F O ()()())
+    )
+  )
+)
 )'''
 ],
 ['retain_path',
@@ -101,6 +211,7 @@ class TestParseAndWrite(unittest.TestCase):
     def setUp(self):
         reconfigure(errorlog=NullLogger);
         self.buf = StringIO();
+        self.maxDiff = None;
 
     def test_testdata(self):
         for data in testdata:
@@ -112,6 +223,4 @@ class TestParseAndWrite(unittest.TestCase):
                 sdfwrite.print_sdf( sdf, channel=self.buf );
                 act = trim_whitespace( self.buf.getvalue() );
                 self.assertEqual( act, exp );
-
-
 
